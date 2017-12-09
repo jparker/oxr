f47e91ad70586e4390c6ce2cab368af65bfbd4d8
Aloïs Thévenot <aloisthevenot@srxp.com>
2017-12-09 17:41:50 +0000

    Remove memoization

--- lib/oxr/configuration.rb
+++ lib/oxr/configuration.rb
@@ -8,27 +8,27 @@ module OXR
     attr_writer :currencies, :historical, :latest, :usage
 
     def currencies
-      @currencies ||= URI.join(ENDPOINT, 'currencies.json').tap { |uri|
+      @currencies || URI.join(ENDPOINT, 'currencies.json').tap { |uri|
         uri.query = "app_id=#{app_id}"
       }.to_s
     end
 
     def historical(date)
-      @historical ||= URI.join(ENDPOINT, "historical/#{date.strftime('%F')}.json").tap { |uri|
+      @historical || URI.join(ENDPOINT, "historical/#{date.strftime('%F')}.json").tap { |uri|
         uri.query = "app_id=#{app_id}"
         uri.query += "&base=#{base}" if base
       }.to_s
     end
 
     def latest
-      @latest ||= URI.join(ENDPOINT, 'latest.json').tap { |uri|
+      @latest || URI.join(ENDPOINT, 'latest.json').tap { |uri|
         uri.query = "app_id=#{app_id}"
         uri.query += "&base=#{base}" if base
       }.to_s
     end
 
     def usage
-      @usage ||= URI.join(ENDPOINT, 'usage.json').tap { |uri|
+      @usage || URI.join(ENDPOINT, 'usage.json').tap { |uri|
         uri.query = "app_id=#{app_id}"
       }.to_s
     end
